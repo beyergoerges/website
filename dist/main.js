3bf05d35ab9e186fe12628af2b26b7f8977199d1
joshuabeyer <mail@joshuabeyer.com>
2018-09-16 23:56:35 +0200

    grid + base layout setup, agency page, portfolio page

--- dist/main.js
+++ dist/main.js
@@ -44,13 +44,7 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-	var box = document.getElementById("jd-claim");
-	var animationDurationInSeconds = 1.5;
-	
-	TweenMax.to(box, animationDurationInSeconds, {
-	  letterSpacing: "-.05em",
-	  ease: "Elastic.easeOut"
-	});
+
 
 /***/ })
 /******/ ]);
--- src/main.js
+++
@@ -1,7 +0,0 @@
-var box = document.getElementById("jd-claim");
-var animationDurationInSeconds = 1.5;
-
-TweenMax.to(box, animationDurationInSeconds, {
-  letterSpacing: "-.05em",
-  ease: "Elastic.easeOut"
-});
