fb80e8acba47c8f38b29dd50155388dee209a82a
joshuabeyer <mail@joshuabeyer.com>
2018-11-08 12:26:20 +0100

    changed page structure (projects first), new sublines, next links

--- src/07-themes/_themes.js
+++ src/07-themes/_themes.js
@@ -13,10 +13,10 @@ let jd_sunsetTime = jd_sunset.getTime();
 
 const jd_body = document.body;
 
-// if (jd_currentDate >= jd_sunrise && jd_currentDate < jd_sunset) {
-//   jd_body.classList.remove("jd-theme--night");
-//   jd_body.classList.add("jd-theme--day");
-// } else {
-//   jd_body.classList.remove("jd-theme--day");
-//   jd_body.classList.add("jd-theme--night");
-// }
+if (jd_currentDate >= jd_sunrise && jd_currentDate < jd_sunset) {
+  jd_body.classList.remove("jd-theme--night");
+  jd_body.classList.add("jd-theme--day");
+} else {
+  jd_body.classList.remove("jd-theme--day");
+  jd_body.classList.add("jd-theme--night");
+}
