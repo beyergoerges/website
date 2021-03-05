1964468e140958b2d5bb1931e0d95e9fab5ef7d1
joshuabeyer <mail@joshuabeyer.com>
2021-03-05 14:01:34 +0100

    hero style update + english page version

--- src/05-layout/_block.js
+++ src/05-layout/_block.js
@@ -6,10 +6,10 @@ jd_scene(".jd-block--intro .jd-heading--page").reverse(false).on("enter", event 
   );
 });
 
-jd_scene(".jd-block--intro .jd-heading--high-level").reverse(false).on("enter", event => {
+jd_scene(".jd-block--intro .jd-heading--section").reverse(false).on("enter", event => {
   let jd_tl = new TimelineLite();
   jd_tl.fromTo(
-    ".jd-block--intro .jd-heading--high-level",
+    ".jd-block--intro .jd-heading--section",
     1.3, {x: "-16px", opacity: 0}, {x: 0, opacity: 1}
   );
 });
