26b250801992119bce713635c118a4e31ff110ad
joshuabeyer <mail@joshuabeyer.com>
2021-03-20 01:37:44 +0100

    updated jobs intro text style

--- src/06-templates/_employer.js
+++ src/06-templates/_employer.js
@@ -3,7 +3,7 @@ jd_scene(".jd-employer").reverse(false).on("enter", event => {
   let jd_tl = new TimelineLite();
 
   jd_tl.staggerFromTo(
-    ".jd-employer__description .jd-paragraph",
+    ".jd-employer__description .jd-heading--high-level",
     .4, {x: "-16px", opacity: 0}, {x: 0, opacity: 1}, .2
   );
 
